23c59bc67b9cf45decef6e8682a0081e8de80a72
Wayakuyee <tdliu8@gmail.com>
2018-08-15 15:49:58 -0400

    Fixed game screen calculation

--- SuperSmashFamilyMelee/app/src/main/java/clubcubed/supersmashfamilymelee/MainActivity.java
+++ SuperSmashFamilyMelee/app/src/main/java/clubcubed/supersmashfamilymelee/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
 
         Global.GAME_HEIGHT = 500f;
         Global.GAME_RATIO = Global.GAME_HEIGHT/Global.SCREEN_HEIGHT;
-        Global.GAME_WIDTH = Global.GAME_HEIGHT * Global.GAME_RATIO;
+        Global.GAME_WIDTH = Global.SCREEN_WIDTH * Global.GAME_RATIO;
 
         // asks for file access permissions
         // probably NOT NEEDED for melee
